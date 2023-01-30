9da5a2acf460fc245ccb7321b62188ccf5958a5a
Sabudh Bahadur Thapa <tsabudh@gmail.com>
2023-01-30 12:51:46 +0545

    js04 removed extra image

---
+++ js-course/js-assignment-05/scripts/js-05.js
@@ -0,0 +1,13 @@
+// global variable
+const WIDTH_OF_CANVAS = 500;
+const HEIGHT_OF_CANVAS = 500;
+const WIDTH = 80;
+const HEIGHT = 80;
+const NUMBER_OF_ANTS = 3;
+// function to create elements
+function createElement(tagName, className, parentName){
+    let newElement = document.createElement(tagName);
+    element.className= className;
+    document.querySelector(targetName).appendChild(element);
+    return element;
+}
