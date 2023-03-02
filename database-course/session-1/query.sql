339f0a9b674ea19066e1c48da0e385b071ed7f1f
Sabudh Bahadur Thapa <tsabudh@gmail.com>
2023-03-02 11:47:55 +0545

    erd

--- database-course/session-1/query.sql
+++ database-course/session-1/query.sql
@@ -1,7 +1,6 @@
--- Active: 1677131876061@@127.0.0.1@5432@postgres
+-- Active: 1677131859744@@localhost@5432@blog
 
 CREATE TYPE "userRole" AS ENUM('admin', 'author', 'moderator');
-
 
 CREATE TABLE
     "users"(
@@ -23,7 +22,7 @@ CREATE TYPE "postStatus" AS ENUM('post','draft');
 CREATE TABLE
     "posts"(
         "id" SERIAL NOT NULL PRIMARY KEY,
-		"title" VARCHAR(250),
+        "title" VARCHAR(250),
         "content" TEXT,
         "status" "postStatus" NOT NULL,
         "userId" INT REFERENCES users(id),
@@ -37,6 +36,8 @@ CREATE TABLE
         "views" INTEGER DEFAULT 0,
         "isFeatured" BOOLEAN DEFAULT false
     );
+
+drop table "postMetaData";
 
 -- Table for comments and reply
 
@@ -73,6 +74,7 @@ CREATE TABLE
         "tagId" INTEGER REFERENCES tags(id),
         PRIMARY KEY ("postId", "tagId")
     );
+
 -- Catergories the posts
 
 CREATE TABLE
@@ -84,9 +86,9 @@ CREATE TABLE
 
 CREATE Table
     "categorizedPosts"(
-        postId INTEGER REFERENCES posts(id),
-        categoryId INTEGER REFERENCES categories(id),
-		PRIMARY KEY (postId, categoryId)
+        "postId" INTEGER REFERENCES posts(id),
+        "categoryId" INTEGER REFERENCES categories(id),
+        PRIMARY KEY ("postId", "categoryId")
     );
 
 -- 1. insert the users
@@ -144,13 +146,19 @@ VALUES (
     );
 
 -- 2. creating new post with draft
+
 ALTER TABLE posts ADD COLUMN "title" VARCHAR(250) NOT NULL;
 
 INSERT INTO
-    "posts"("title","content", "status", "userId")
+    "posts"(
+        "title",
+        "content",
+        "status",
+        "userId"
+    )
 VALUES (
         'What I do',
-		'I watch youtube videos on current and future tech and gadgets whenever I am feeling tired. My interest being anything related to computers from microphone gadgets to quantumn computers, these videos turn my frown upside down and they are informative too. I have subscribed to various tech channels like MKBHD,  Just Josh, Gamer Meld and many more. I find videos more entertaining than musics. It is always better to be tranquilized by two senses than one. The new innovations and developments happen almost daily and new goals are achieved and another sets of goals are created. It reminds me that the world does not stop for me to get back to a good  mood. It always moves forward in a pace that is increasing rapidly. Such videos remind me what a blessing it is to be born in this era and the astronomical achievements humans have attained compared to the Stone Age where lighting up fire was deemed as magical.',
+        'I watch youtube videos on current and future tech and gadgets whenever I am feeling tired. My interest being anything related to computers from microphone gadgets to quantumn computers, these videos turn my frown upside down and they are informative too. I have subscribed to various tech channels like MKBHD,  Just Josh, Gamer Meld and many more. I find videos more entertaining than musics. It is always better to be tranquilized by two senses than one. The new innovations and developments happen almost daily and new goals are achieved and another sets of goals are created. It reminds me that the world does not stop for me to get back to a good  mood. It always moves forward in a pace that is increasing rapidly. Such videos remind me what a blessing it is to be born in this era and the astronomical achievements humans have attained compared to the Stone Age where lighting up fire was deemed as magical.',
         'draft',
         1
     );
@@ -158,18 +166,27 @@ VALUES (
 -- 3. publich new post
 
 INSERT INTO
-    "posts"("title","content", "status", "userId")
+    "posts"(
+        "title",
+        "content",
+        "status",
+        "userId"
+    )
 VALUES (
         'Rest API',
         'What is a REST API? An API, or application programming interface, is a set of rules that define how applications or devices can connect to and communicate with each other. A REST API is an API that conforms to the design principles of the REST, or representational state transfer architectural style.',
-	'post',
+        'post',
         2
     );
 
 -- 4 add comment
 
 INSERT INTO
-    "comments"("commentText", "userId", "postId")
+    "comments"(
+        "commentText",
+        "userId",
+        "postId"
+    )
 VALUES (
         'This should not be here.',
         1,
@@ -193,13 +210,26 @@ VALUES (
 -- 5. retriving post by  categories
 
 SELECT
-    posts.content,
-    categories.name,
-    categories.description
-FROM posts
-    INNER JOIN categories ON "posts"."id" = "categorizedPost"."postId";
+    p.id,
+    p.title,
+    p.content
+FROM posts p
+    JOIN "categorizedPosts" cp ON p.id = cp."postId"
+    JOIN categories c ON cp."categoryId" = c.id
+WHERE c.name = 'sports';
 
--- trending posts
+-- 6. retriving featured post
+
+SELECT
+    posts.id,
+    posts.title,
+    posts.content
+FROM posts
+    INNER JOIN "postMetaData" ON posts.id = "postMetaData"."postId"
+WHERE
+    "postMetaData"."isFeatured" = true;
+
+-- 7. trending posts with  views greater than 10
 
 SELECT
     posts.content,
@@ -208,8 +238,4 @@ SELECT
     "postMetaData"."isFeatured"
 FROM posts
     INNER JOIN "postMetaData" ON posts.id = "postMetaData"."postId"
-WHERE "postMetaData".views = (
-        SELECT
-            MAX("postMetaData".views)
-        FROM "postMetaData"
-    );
+WHERE "postMetaData".views > 10
--- database-course/session-2/query.sql
+++ database-course/session-2/query.sql
@@ -1,12 +1,12 @@
 -- Active: 1677410332760@@127.0.0.1@5432@postgres@public
 CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
+    "userId" SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW(),
-    deleted_at TIMESTAMP
+    "createdAt" TIMESTAMP DEFAULT NOW(),
+    "updatedAt" TIMESTAMP DEFAULT NOW(),
+    "deletedAt" TIMESTAMP
 );
 
 -- insert the users
@@ -19,60 +19,60 @@ VALUES('Sailesh','sailesh123','Sailesh47@gmail.com');
 
 
 CREATE TABLE tweets (
-    tweet_id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
+    "tweetId" SERIAL PRIMARY KEY,
+    "userId" INTEGER REFERENCES users("userId") ON DELETE CASCADE,
     content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    num_likes INTEGER DEFAULT 0
+    "createdAt" TIMESTAMP DEFAULT NOW(),
+    "likesCount" INTEGER DEFAULT 0
 );
 
-INSERT INTO tweets(user_id,content,num_likes)
+INSERT INTO tweets("userId",content,"likesCount")
 VALUES(1,'its new tweet',4);
 
-INSERT INTO tweets(user_id,content,num_likes)
+INSERT INTO tweets("userId",content,"likesCount")
 VALUES(1,'its second tweet',8);
 
 
 CREATE TABLE followers (
-    follower_id SERIAL PRIMARY KEY,
-    follower_user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
-    following_user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE
+    "followerId" SERIAL PRIMARY KEY,
+    follower_"userId" INTEGER REFERENCES users("userId") ON DELETE CASCADE,
+    following_"userId" INTEGER REFERENCES users("userId") ON DELETE CASCADE
 );
 
 
 
 -- insert follower
 
-INSERT INTO followers(follower_user_id,following_user_id)
+INSERT INTO followers(follower_"userId",following_"userId")
 VALUES(1,2);
 
 CREATE TABLE followings(
-    user_id INT NOT NULL,
+    "userId" INT NOT NULL,
     following_id INT NOT NULL,
-    Foreign Key (user_id) REFERENCES users(id) ON DELETE NO ACTION,
+    Foreign Key ("userId") REFERENCES users(id) ON DELETE NO ACTION,
     Foreign Key (following_id) REFERENCES users(id) ON DELETE NO ACTION
 );
 
 -- followed by the user
 INSERT INTO
-    followings(user_id, following_id)
+    followings("userId", following_id)
 VALUES
     (1, 2);
 
 
 CREATE TABLE comments (
     comment_id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
-    tweet_id INTEGER REFERENCES tweets(tweet_id) ON DELETE CASCADE,
+    "userId" INTEGER REFERENCES users("userId") ON DELETE CASCADE,
+    "tweetId" INTEGER REFERENCES tweets("tweetId") ON DELETE CASCADE,
     content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    num_likes INTEGER DEFAULT 0
+    "createdAt" TIMESTAMP DEFAULT NOW(),
+    "likesCount" INTEGER DEFAULT 0
 );
 
 
 -- comment in a tweet
 INSERT INTO
-    comments(tweet_id, user_id, content,num_likes)
+    comments("tweetId", "userId", content,"likesCount")
 VALUES
     (1, 2, 'this is commented by user 2',5);
 
@@ -86,9 +86,9 @@ CREATE TABLE hashtags (
 
 
 CREATE TABLE tweet_hashtags (
-    tweet_id INTEGER REFERENCES tweets(tweet_id) ON DELETE CASCADE,
+    "tweetId" INTEGER REFERENCES tweets("tweetId") ON DELETE CASCADE,
     hashtag_id INTEGER REFERENCES hashtags(hashtag_id) ON DELETE CASCADE,
-    PRIMARY KEY (tweet_id, hashtag_id)
+    PRIMARY KEY ("tweetId", hashtag_id)
 );
 
 
